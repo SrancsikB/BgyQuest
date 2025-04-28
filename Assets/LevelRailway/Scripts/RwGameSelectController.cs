ea224d6c5eee7124da35ba1a437937b32e3e31fc
SrancsikB <balint.srancsik@gmail.com>
2025-04-28 16:24:59 +0200

    Map select

---
+++ Assets/LevelRailway/Scripts/RwGameSelectController.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class RwGameSelectController : MonoBehaviour
+{
+    [SerializeField] PlayerDataControl.RailwayPogressData rwPD;
+    [SerializeField] GameObject canvasUI;
+
+    public int coinQuantity;
+
+
+    void Awake()
+    {
+        canvasUI.SetActive(true);
+
+        try //try to get Player data
+        {
+
+            PlayerDataControl pDC = PlayerDataControl.Instance;
+            coinQuantity = pDC.coins;
+            rwPD = pDC.GetRailwayProgressData();
+
+
+        }
+        catch (System.Exception)
+        {
+            coinQuantity = 0;
+            Debug.Log("SB: Player data cannot be aquired, using public progress data");
+        }
+    }
+
+
+    private void Start()
+    {
+        UILevelSelector[] levels = FindObjectsByType<UILevelSelector>(FindObjectsSortMode.None);
+        foreach (UILevelSelector level in levels)
+        {
+            if (level.levelNumber > rwPD.rwMapLevel)
+            {
+                level.GetComponent<Collider2D>().enabled = false;
+                level.transform.GetChild(0).GetChild(0).GetComponent<TMPro.TextMeshProUGUI>().color = Color.red;
+            }
+        }
+    }
+
+
+    void Update()
+    {
+        FindFirstObjectByType<UICoin>().coinQuantity = coinQuantity;
+    }
+}
---
+++ Assets/LevelRailway/Scripts/UILevelSelector.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class UILevelSelector : MonoBehaviour
+{
+    public int levelNumber = 1;
+}
